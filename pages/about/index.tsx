9a115d61d8a7deda5b3567cad30266932f50716a
ikeura <ikeryo1182@gmail.com>
2019-01-17 15:31:34 +0900

    eslintとprettierの導入

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -2,20 +2,20 @@ import * as React from "react";
 
 import style from "./index.scss";
 
-import DefaultLayout from '../../src/components/layouts/default'
+import DefaultLayout from "../../src/components/layouts/default";
 import CustomHead from "../../src/components/head/index";
 
 class AboutPage extends React.Component<{}> {
-    public render() {
-        return (
-            <>
-                <CustomHead pathname="about" />
-                <DefaultLayout>
-                    <div className={style.content}>Welcome to about!</div>
-                </DefaultLayout>
-            </>
-        )
-    }
+  public render() {
+    return (
+      <>
+        <CustomHead pathname="about" />
+        <DefaultLayout>
+          <div className={style.content}>Welcome to about!</div>
+        </DefaultLayout>
+      </>
+    );
+  }
 }
 
-export default AboutPage
+export default AboutPage;
