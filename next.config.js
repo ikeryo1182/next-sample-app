2c708befd0388ff7147bbe86c912f9a1b870b742
ikeura <ikeryo1182@gmail.com>
2019-03-01 14:20:07 +0900

    upgrade some module versions and add compressing module

--- next.config.js
+++ next.config.js
@@ -1,13 +1,36 @@
-const withTypescript = require('@zeit/next-typescript')
-const withSass = require('@zeit/next-sass')
-const path = require('path')
+const withTypescript = require("@zeit/next-typescript");
+const withSass = require("@zeit/next-sass");
+
+const CompressionPlugin = require("compression-webpack-plugin");
+const zopfli = require("@gfx/zopfli");
+const brotli = require("iltorb");
+
+const production = process.env.NODE_ENV === "production";
 
 module.exports = withTypescript(
   withSass({
-    webpack(config, options) {
+    webpack(config) {
+      production &&
+        config.plugins.push(
+          new CompressionPlugin({
+            test: /\.(js|css)$/,
+            // eslint-disable-line no-unused-vars
+            algorithm: (input, options, callback) => {
+              zopfli.gzip(input, {}, callback);
+            }
+          }),
+          new CompressionPlugin({
+            test: /\.(js|css)$/,
+            filename: "[path].br[query]",
+            // eslint-disable-line no-unused-vars
+            algorithm: (input, options, callback) => {
+              brotli.compress(input, {}, callback);
+            }
+          })
+        );
       return config;
     },
     cssModules: true,
-    sassLoaderOptions: {},
+    sassLoaderOptions: {}
   })
 );
